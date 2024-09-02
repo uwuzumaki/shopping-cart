b22c6960dcd1c412f579a89a7cdc4e3e7a94dc0a
Kris Wong <krischwong@gmail.com>
2024-09-02 18:10:16 -0400

    Added error routing

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,19 @@
 import App from "./App";
 import Homepage from "./Components/Homepage";
 import Shop from "./Components/Shoppage";
+import Error from "./Components/Errorpage";
+import Navbar from "./Components/Navbar";
 
 const routes = [
   {
     path: "/",
     element: <App />,
+    errorElement: (
+      <>
+        <Navbar />
+        <Error />
+      </>
+    ),
     children: [
       { path: "/", element: <Homepage /> },
       { path: "shop", element: <Shop /> },
