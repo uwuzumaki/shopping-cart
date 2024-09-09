2658b74b9665f0cfac6a311d4021dcf5a8abe8a2
Kris Wong <krischwong@gmail.com>
2024-09-08 20:33:36 -0400

    Added providers for the store and user cart

---
+++ src/Components/ContextProvider.jsx
@@ -0,0 +1,38 @@
+/* eslint-disable react/prop-types */
+import { createContext, useState, useEffect } from "react";
+
+export const StoreDataContext = createContext(null);
+export const UserCartContext = createContext(null);
+
+const ContextProvider = ({ children }) => {
+  const [store, setStore] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch("https://fakestoreapi.com/products");
+        if (!data.ok) {
+          throw new Error(`HTTP Error: Status ${data.status}`);
+        }
+        const json = await data.json();
+        console.log(json);
+        setStore(json);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <StoreDataContext.Provider value={{ store, setStore }}>
+      <UserCartContext.Provider value={{ cart, setCart }}>
+        {children}
+      </UserCartContext.Provider>
+    </StoreDataContext.Provider>
+  );
+};
+
+export default ContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes.jsx";
+import ContextProvider from "./Components/ContextProvider.jsx";
 
 const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <ContextProvider>
+      <RouterProvider router={router} />
+    </ContextProvider>
   </StrictMode>
 );
