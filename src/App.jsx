ecca28bdf96bbecb9c9412c952511c92920a84ee
Kris Wong <krischwong@gmail.com>
2024-09-09 20:41:48 -0400

    removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
-/* eslint-disable no-unused-vars */
-import { useState } from "react";
 import "./App.css";
-import Homepage from "./Components/Homepage";
 import Navbar from "./Components/Navbar";
 import { Outlet } from "react-router-dom";
 
